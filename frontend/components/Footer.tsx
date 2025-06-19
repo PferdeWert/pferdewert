b0457edf8f8553077c91cd191d92bed24f1e24bc
PferdeWert <benni_reder@gmx.de>
2025-06-19 07:30:39 +0000

    footer#

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -1,10 +1,15 @@
-// components/Footer.tsx
+// frontend/components/Footer.tsx
+import Link from "next/link";
 
 export default function Footer() {
   return (
     <footer className="text-center text-sm text-gray-500 mt-12 py-6 border-t">
-      <a href="/impressum" className="hover:underline mx-2">Impressum</a>
-      <a href="/datenschutz" className="hover:underline mx-2">Datenschutz</a>
+      <Link href="/impressum" className="hover:underline mx-2">
+        Impressum
+      </Link>
+      <Link href="/datenschutz" className="hover:underline mx-2">
+        Datenschutz
+      </Link>
     </footer>
   );
 }
