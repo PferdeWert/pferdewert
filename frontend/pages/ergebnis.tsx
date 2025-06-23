274ceec701cf7561fc698ab6fa6360a26d462983
PferdeWert <benni_reder@gmx.de>
2025-06-23 09:45:37 +0000

    pdf

--- frontend/pages/ergebnis.tsx
+++ frontend/pages/ergebnis.tsx
@@ -88,47 +88,16 @@ export default function Ergebnis() {
     .replace(/\*\*(.*?)\*\*/gim, "$1:")
     .replace(/\n{2,}/g, "\n\n");
 
-  const img = new Image();
-  img.src = "/logo.png";
-  img.onload = () => {
-    const pageWidth = 210;
-    const pageHeight = 297;
-    const margin = 15;
-    const lineHeight = 6;
-    const lines = pdf.splitTextToSize(body, pageWidth - 2 * margin);
-
-    let cursorY = 70;
-
-    const addPageFooter = (pageNum: number, totalPages: number) => {
-      pdf.setFontSize(10);
-      pdf.text(`© PferdeWert 2025 – www.pferdewert.de`, margin, pageHeight - 10);
-      pdf.text(`${pageNum} / ${totalPages}`, pageWidth - margin, pageHeight - 10, { align: "right" });
-    };
-
-    pdf.addImage(img, "PNG", (pageWidth - 50) / 2, 10, 50, 15);
-    pdf.setFont("times", "normal");
-    pdf.setLineHeightFactor(1.6);
-    pdf.setFontSize(16);
-    pdf.text("Pferdebewertung", pageWidth / 2, 35, { align: "center" });
-    pdf.setFontSize(12);
-    pdf.text(headerText, margin, 45);
-
-    let pageNum = 1;
-    for (let i = 0; i < lines.length; i++) {
-      if (cursorY + lineHeight > pageHeight - 20) {
-        addPageFooter(pageNum, 1); // Temporär
-        pdf.addPage();
-        pageNum++;
-        cursorY = 20;
-      }
-      pdf.text(lines[i], margin, cursorY);
-      cursorY += lineHeight;
-    }
-
-    addPageFooter(pageNum, pageNum);
-    pdf.save("pferdebewertung.pdf");
-  };
+  const lines = pdf.splitTextToSize(body, 180);
+  pdf.setFont("times", "normal");
+  pdf.setFontSize(12);
+  pdf.setLineHeightFactor(1.6);
+  pdf.text("Pferdebewertung", 105, 20, { align: "center" });
+  pdf.text(headerText, 10, 30);
+  pdf.text(lines, 10, 50);
+  pdf.save("test.pdf");
 };
+
 
 
 
