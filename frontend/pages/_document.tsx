22d8afd03f48048695c201ebae3e2618c12edb56
PferdeWert <benni_reder@gmx.de>
2025-06-24 21:32:36 +0000

    c

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -1,3 +1,4 @@
+// frontend/pages/_document.tsx
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
@@ -5,57 +6,9 @@ export default function Document() {
     <Html lang="de">
       <Head>
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
-        {/* Silktide Consent Manager (Open Source Version) */}
         <link
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.css"
-        />
-        <script
-          src="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.js"
-          data-cfasync="false"
-        ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.addEventListener("load", function(){
-                window.cookieconsent.initialise({
-                  palette: {
-                    popup: {
-                      background: "#fff",
-                      text: "#000"
-                    },
-                    button: {
-                      background: "#007bff",
-                      text: "#fff"
-                    }
-                  },
-                  theme: "classic",
-                  position: "bottom-right",
-                  type: "opt-in",
-                  content: {
-                    message: "Diese Website verwendet Cookies, um Ihr Erlebnis zu verbessern.",
-                    dismiss: "Nur notwendige",
-                    allow: "Alle akzeptieren",
-                    deny: "Ablehnen",
-                    link: "Mehr erfahren",
-                    href: "/datenschutz"
-                  },
-                  onInitialise: function (status) {
-                    if (this.hasConsented()) {
-                      // Cookies aktivieren (z. B. Google Analytics)
-                    }
-                  },
-                  onStatusChange: function(status, chosenBefore) {
-                    if (this.hasConsented()) {
-                      // Cookies aktivieren
-                    } else {
-                      // Cookies blockieren
-                    }
-                  }
-                });
-              });
-            `
-          }}
         />
       </Head>
       <body className="antialiased">
