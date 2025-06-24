8ab2fbef24de3403ba6aa06c546e21b97d887119
PferdeWert <benni_reder@gmx.de>
2025-06-24 22:01:11 +0000

    c

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -37,7 +37,7 @@ export default function App({ Component, pageProps }: AppProps) {
               text: "#fff",
             },
           },
-          theme: "edgeless",
+theme: "classic",
           position: "middle",
           type: "opt-in",
           content: {
