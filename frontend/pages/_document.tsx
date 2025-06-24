77928835b579ec02e9cf56a8a1a50f47c46a3788
PferdeWert <benni_reder@gmx.de>
2025-06-24 21:10:27 +0000

    cookie banner eingefügrt

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -2,9 +2,58 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="de">
       <Head>
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
+        
+        {/* Silktide Consent Manager */}
+        <script
+          id="cookieconsent"
+          src="https://cookieconsent.popupsmart.com/src/js/popper.js"
+          type="text/javascript"
+          async
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.cookieconsent.initialise({
+                palette: {
+                  popup: {
+                    background: "#fff",
+                    text: "#000"
+                  },
+                  button: {
+                    background: "#007bff",
+                    text: "#fff"
+                  }
+                },
+                theme: "classic",
+                position: "bottom-right",
+                type: "opt-in",
+                content: {
+                  message: "Diese Website verwendet Cookies, um Ihr Erlebnis zu verbessern.",
+                  dismiss: "Nur notwendige",
+                  allow: "Alle akzeptieren",
+                  deny: "Ablehnen",
+                  link: "Mehr erfahren",
+                  href: "/datenschutz"
+                },
+                onInitialise: function (status) {
+                  if (this.hasConsented()) {
+                    // Cookies aktivieren, z.B. Google Analytics
+                  }
+                },
+                onStatusChange: function(status, chosenBefore) {
+                  if (this.hasConsented()) {
+                    // Cookies aktivieren
+                  } else {
+                    // Cookies blockieren
+                  }
+                }
+              });
+            `
+          }}
+        />
       </Head>
       <body className="antialiased">
         <Main />
