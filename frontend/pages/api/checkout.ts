f05fb4776fda406ee5d4353ecba3757e1f324306
PferdeWert <benni_reder@gmx.de>
2025-06-27 07:17:44 +0000

    klarna

--- frontend/pages/api/checkout.ts
+++ frontend/pages/api/checkout.ts
@@ -58,7 +58,7 @@ try {
     const origin = process.env.NEXT_PUBLIC_BASE_URL!;
 
     const session = await stripe.checkout.sessions.create({
-      payment_method_types: ["card"],
+      payment_method_types: ["card", "klarna"],
       line_items: [{ price: process.env.STRIPE_PRICE_ID!, quantity: 1 }],
       mode: "payment",
       success_url: `${origin}/ergebnis?session_id={CHECKOUT_SESSION_ID}`,
