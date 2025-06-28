99697d8bd758a5c8ad5de8ae84d33ca7bd351a20
PferdeWert <benni_reder@gmx.de>
2025-06-28 19:55:01 +0000

    _app und _doc

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -5,10 +5,23 @@ export default function Document() {
   return (
     <Html lang="de">
       <Head>
-<link rel="stylesheet" href="/css/cookieconsent.min.css?v=20250628" />
+        <link rel="stylesheet" href="/css/cookieconsent.min.css?v=20250628" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
       </Head>
       <body className="antialiased">
+        <noscript>
+          <div style={{
+            background: '#fff',
+            color: '#000',
+            padding: '1rem',
+            textAlign: 'center',
+            fontSize: '14px',
+            fontFamily: 'sans-serif',
+            zIndex: 9999
+          }}>
+            Diese Website benötigt JavaScript für die Cookie-Einstellungen. Bitte aktivieren Sie JavaScript in Ihrem Browser.
+          </div>
+        </noscript>
         <Main />
         <NextScript />
       </body>
